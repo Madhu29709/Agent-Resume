4d1da7ba411367012f7beba9d1b7aa802bb83e08
Madhu29709 <madhu.codes29@gmail.com>
2026-07-30 10:24:46 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -16,19 +16,18 @@ import numpy as np
 
 
 # to show the web-app:complete page layout
-st.set_page_config(layout-wide)
+st.set_page_config(layout = 'wide')
 
 # TO give title
 st.title("AI RESUME GENERATOR")
 
 st.write("""This app helps user to build customized professional Resume with latest Job Apply Link""")
-st.image("")
+st.image("bg.png")
 # ====API kEYS========
 
 GOOGLE_API_KEY ="AQ.Ab8RN6IZ7tWDZnFe3AH6xnSM-rMV1NoO5eUasIm8SxZ_HnS0Dg"
 GROQ_API_KEY = "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"
 TAVILY_API_KEY = "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"
-print("Done")
 
 # ======MODELS==========
 model = ChatGoogleGenerativeAI(
@@ -36,8 +35,8 @@ model = ChatGoogleGenerativeAI(
     google_api_key = GOOGLE_API_KEY
 
 )
-response = model.invoke("Hello Buddy!")
-response.content[-1]['text']
+#response = model.invoke("Hello Buddy!")
+#response.content[-1]['text']
 
 
 # =====TOOLS========
@@ -58,7 +57,7 @@ agent = create_agent(
     model = model,
     tools = [search_lastest_news_jobs])
 
-agent
+#agent
 
 
 # =====models======
